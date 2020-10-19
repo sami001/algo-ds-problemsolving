a190b1c2561db0c5abd2af5ae7fef1796784a788
Mohammed Samiul Saeef <saeef.sami@gmail.com>
2020-10-19 01:52:04 -0500

    Create lc701.py

---
+++ lc701.py
@@ -0,0 +1,19 @@
+#binarytree
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def insertIntoBST(self, root: TreeNode, val: int) -> TreeNode:
+        if root == None:
+            root = TreeNode(val, left=None, right=None)
+            return root
+        if val > root.val:
+            root.right = self.insertIntoBST(root.right, val)
+        else:
+            root.left = self.insertIntoBST(root.left, val)
+        return root
+            
