18c872bdeceeea0787d0cbaa00c10455ea6eb228
Mohammed Samiul Saeef <saeef.sami@gmail.com>
2020-10-21 21:10:43 -0500

    Update lc1481.py

--- lc1481.py
+++ lc1481.py
@@ -11,17 +11,14 @@ class Solution:
         for key in cnt:
             elem_cnt.append(cnt[key])
         elem_cnt.sort()
-        
-        #print(elem_cnt)
-        
+   
         for i in range(len(elem_cnt)):
-            #print(k)
             temp = elem_cnt[i]
             elem_cnt[i] = max(0, elem_cnt[i]-k)
             k = max(0, k-temp)
             
             if k == 0:
                 break
-        #print(elem_cnt)
+                
         return len([x for x in elem_cnt if x > 0])
             
