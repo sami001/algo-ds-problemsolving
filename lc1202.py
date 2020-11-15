5116e4aaf06d78304bb0016a60682b302320d9f0
Mohammed Samiul Saeef <saeef.sami@gmail.com>
2020-11-14 20:40:12 -0600

    Create lc1202.py

---
+++ lc1202.py
@@ -0,0 +1,51 @@
+#https://leetcode.com/problems/smallest-string-with-swaps/submissions/
+
+#unionfind
+#disjointset
+
+
+class Solution:
+
+    def smallestStringWithSwaps(self, s: str, pairs: List[List[int]]) -> str:
+            
+        rank = [1 for i in range(100000)]
+        parent = [i for i in range(100000)]
+        
+        def find(v):
+            if parent[v] == v: return v
+            return find(parent[v])
+    
+        def union(u, v):
+            u = find(u)
+            v = find(v)
+
+            if u == v: return
+            if rank[u] > rank[v]: u, v = v, u
+            rank[v] += rank[u]
+            parent[u] = v
+
+        for p in pairs:
+            union(p[0], p[1])
+        
+        N = len(s)
+        dict = {}
+        for i in range(N):
+            parent[i] = find(i)
+            if parent[i] in dict:
+                dict[parent[i]].append(s[i])
+            else:
+                dict[parent[i]] = [s[i]]
+        
+        for k in dict:
+            dict[k].sort()
+
+        ans = ''
+        
+        for i in range(len(s)):
+            ans += dict[parent[i]][0]
+            dict[parent[i]].pop(0)
+        
+        return ans
+            
+    
+    
